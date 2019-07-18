507c2fe42ab3f5f17bfdec030dc81254c32d881c
kanji0028 <bukurabo@gmail.com>
2019-07-18 18:40:16 +0900

    2019/7/18

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,16 +1,42 @@
 $(function(){
-  $('#new_message').on('submit',function(e){
-    e.preventDefault();
-    console.log(this)
-    var formData = new FormData(this);
-    var url = $(this).attr('action')
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
+  function buildHTML(message){
+    var html = `<div class="message">
+                  <div class="message__upper-info">
+                    <div class="message__upper-info__talker">
+                    ${message.user_name}
+                    </div>
+                    <div class="message__upper-info__date">
+                    ${message.created_at}
+                    </div>
+                    </div>
+                    <div class="message__text"></div>
+                    <p class="message__text__content">
+                    ${message.content}
+                    </p>
+                    <img class="message__text__image" src="/uploads/message/image/${message.id}/${message.image}" alt="Profy">
+                  </div>`
+    return html;
+  }
+    $('#new_message').on('submit', function(e){
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action')
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
+      .done(function(data){
+        console.log("成功")
+        var html = buildHTML(data);
+        $('.messages').append(html)
+        $('.input-box__text').val('')
+      })
+      .fail(function(){
+        alert("error");
     })
   })
-})
+});
--- app/views/messages/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.content     @message.content
-json.image       @message.image
-json.group_id    @message.group.id
-json.user_id     @message.user.id
