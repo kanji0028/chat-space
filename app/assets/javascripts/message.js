e8a670df843b0c64fe103c43a53b8dd9689a8eec
kanji0028 <bukurabo@gmail.com>
2019-07-19 12:28:23 +0900

    API created

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,21 +1,22 @@
 $(function(){
   function buildHTML(message){
+    image = (message.image) ? `<img class= "message__text__image" src=${message.image} >` : "";
+
     var html = `<div class="message">
                   <div class="message__upper-info">
                     <div class="message__upper-info__talker">
                     ${message.user_name}
                     </div>
                     <div class="message__upper-info__date">
-                    ${message.created_at}
+                    ${message.date}
                     </div>
                     </div>
                     <div class="message__text"></div>
                     <p class="message__text__content">
                     ${message.content}
                     </p>
-                    <img class="message__text__image" src="${message.image}">
+                    ${image}
                   </div>`
-    console.log(html)
     return html;
   }
     $('#new_message').on('submit', function(e){
@@ -32,7 +33,6 @@ $(function(){
         contentType: false
       })
       .done(function(data){
-        console.log("成功")
         var html = buildHTML(data);
         $('.messages').append(html);
         $('.messages').animate({
--- app/models/message.rb
+++ app/models/message.rb
@@ -5,4 +5,9 @@ class Message < ApplicationRecord
   validates :content, presence: true, unless: :image?
 
   mount_uploader :image, ImageUploader
+
+  def simple_time
+    time.strftime("%Y/%m/%d %H:%M")
+  end
+
 end
