463212b36fb7044cff8e19adb13153c4ff616120
kanji0028 <bukurabo@gmail.com>
2019-07-19 17:31:53 +0900

    〜８

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,44 @@
 $(function(){
+
+  var search_list = $("#user-search-result");
+
+  function appendUsers(user){
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>`
+    search_list.append(html);
+  }
+
+  function appendNoUsers(user){
+    var html =`<div class="chat-group-user clearfix">
+                <p class="chat-group-user__name">${ user }</p>
+              </div>`
+    search_list.append(html);
+  }
+
   $("#user-search-field").on("keyup" , function(){
     var input = $("#user-search-field").val();
     console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+    .done(function(users){
+      $("#user-search-result").empty();
+      if (users.length !==0){
+        users.forEach(function(user){
+          appendUsers(user);
+        });
+      } else {
+          appendNoUsers("一致するユーザーはいません");
+      }
+    })
+    .fail(function(){
+      alert('検索に失敗しました');
+    })
   })
 });
