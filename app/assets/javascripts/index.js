d215d5dfac7108a7dffdbe2804d0215501cb7dd5
kanji0028 <bukurabo@gmail.com>
2019-07-19 19:43:28 +0900

    Created MentalSearch(1)

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,6 +1,7 @@
 $(function(){
 
   var search_list = $("#user-search-result");
+  var member_list = $("#member-search-result");
 
   function appendUsers(user){
     var html = `<div class="chat-group-user clearfix">
@@ -8,6 +9,16 @@ $(function(){
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                 </div>`
     search_list.append(html);
+    return html;
+  }
+
+  function appendMembers(name, user_id) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                 <input name="group[user_ids]" type="hidden" value="${ user_id }">
+                 <p class="chat-group-user__name">${ name }</p>
+                 <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    member_list.append(html);
   }
 
   function appendNoUsers(user){
@@ -19,7 +30,6 @@ $(function(){
 
   $("#user-search-field").on("keyup" , function(){
     var input = $("#user-search-field").val();
-    console.log(input);
 
     $.ajax({
       type: 'GET',
@@ -27,18 +37,34 @@ $(function(){
       data: { keyword: input },
       dataType: 'json'
     })
+
     .done(function(users){
       $("#user-search-result").empty();
-      if (users.length !==0){
-        users.forEach(function(user){
-          appendUsers(user);
-        });
-      } else {
-          appendNoUsers("一致するユーザーはいません");
-      }
-    })
+          if (users.length !==0){
+            users.forEach(function(user){
+            appendUsers(user);
+            });
+          }
+          else {
+            appendNoUsers("一致するユーザーはいません");
+          }
+        })
     .fail(function(){
       alert('検索に失敗しました');
-    })
-  })
+      })
+  });
+
+    $(function(){
+      $(document).on("click", ".user-search-add" ,function(){
+        console.log("成功");
+        var name = $(this).data("user-name");
+        var user_id = $(this).data("user-id");
+        $(this).parent().remove();
+        appendMembers(name, user_id);
+      })
+    });
+
+      $(document).on("click",".user-search-remove",function(){
+        $(this).parent().remove();
+      });
 });
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where('name LIKE(?)' , "%#{params[:keyword]}%")
+    @users = User.where('name LIKE(?)' , "%#{params[:keyword]}%").where.not(name:current_user.name)
     respond_to do |format|
       format.html
       format.json
